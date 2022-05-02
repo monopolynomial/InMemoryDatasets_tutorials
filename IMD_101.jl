30f8201458557c2fead699c966b9323d9a9f3e8e
monopolynomial <19833580+monopolynomial@users.noreply.github.com>
2022-05-02 00:54:08 +0000

    Update IMD_101.jl

--- IMD_101.jl
+++ IMD_101.jl
@@ -6,3 +6,8 @@ sort!(data,:x1)
 
 # modify!
 modify!(data,:x1=>sqrt=>:y)
+
+# combine
+data=Dataset(group=[2,1,1,2,2,3],y=rand(6))
+combine(groupby(data,:group),:y=>IMD.sum)
+combine(gatherby(data,:group),:y=>IMD.sum)
