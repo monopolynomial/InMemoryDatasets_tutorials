76992d7ea0a563f9dda1c1dbcbb7e303512d9f83
monopolynomial <19833580+monopolynomial@users.noreply.github.com>
2022-05-03 00:25:56 +0000

    Update IMD_101.jl

--- IMD_101.jl
+++ IMD_101.jl
@@ -11,3 +11,9 @@ modify!(data,:x1=>sqrt=>:y)
 data=Dataset(group=[2,1,1,2,2,3],y=rand(6))
 combine(groupby(data,:group),:y=>IMD.sum)
 combine(gatherby(data,:group),:y=>IMD.sum)
+
+# flatten a data set - this is new feature
+data=Dataset(x=[1,2,3],y=["1,2","10,10","1,2,3,4"])
+f(x)=parse.(Int,split(x,","))
+setformat!(data,:y=>f)
+flatten!(data,:y,mapformats=true)
