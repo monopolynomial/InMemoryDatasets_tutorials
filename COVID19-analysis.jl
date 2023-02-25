8761a036d8162c4e44b0789572990224b971a8ad
monopolynomial <19833580+monopolynomial@users.noreply.github.com>
2023-02-25 01:42:59 +0000

    Update COVID19-analysis.jl

--- COVID19-analysis.jl
+++ COVID19-analysis.jl
@@ -1,1 +1,6 @@
 
+using DLMReader
+import Downloads
+temp_downloaded_file=Downloads.download("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+covid=filereader(temp_downloaded_file,quotechar='"',guessingrows=5)
+covid_time_series=transpose(groupby(covid,[1,2,3,4]),Not([1,2,3,4]),renamerowid=x->Date(x,dateformat"m/d/y")+Year(2000),variable_name="Date",renamecolid=x->"Confirmed")
